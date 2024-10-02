2fbc8e971f766446491a0355937a9a378a3e0d39
Diana Barreto <dianibar@gmail.com>
2024-10-02 11:44:10 +1000

    displaying user profile

---
+++ .vite/deps/aws-amplify.js
@@ -0,0 +1,9 @@
+import {
+  DefaultAmplify
+} from "./chunk-MTPBVMWR.js";
+import "./chunk-JRJZRTFU.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  DefaultAmplify as Amplify
+};
+//# sourceMappingURL=aws-amplify.js.map
